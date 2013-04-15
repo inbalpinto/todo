5e0f9d3338d4f20d7b7e0b3deb150121934d7234
inbalpinto <inbal.pinto@gmail.com>
2013-04-15 20:10:44 +0300

    ex4 submmit

--- TodoListManager/src/il/ac/huji/todolist/Item.java
+++ TodoListManager/src/il/ac/huji/todolist/Item.java
@@ -1,1 +1,26 @@
 package il.ac.huji.todolist;
+
+import java.util.Date;
+
+public class Item implements ITodoItem {
+
+
+	public Item(String name, Date dueDate) {
+		this.name = name;
+		this.dueDate = dueDate;
+	}
+
+	public String name;
+	public Date dueDate;
+	
+	@Override
+	public String getTitle() {
+		return name;
+	}
+	@Override
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+}
+
